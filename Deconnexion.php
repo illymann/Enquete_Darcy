1a6e4714f9ad87bbfa6dea64cfb0a7eec0fc5f2c
illymann <lionel.darcy.28.12.96@gmail.com>
2017-05-07 17:49:37 +0200

    Delete Deconnexion.php

--- Deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['session'] = NULL;//On vide le tableau de session
-	session_destroy(); //On détruit la session
-	header('Location:index.php');//On redirige vers la page de connexion
-	exit();
-?>
